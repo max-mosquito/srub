f6ae66354e7f6b571851c5720b61748fa4373cfc
max-mosquito <67696697+max-mosquito@users.noreply.github.com>
2020-10-26 15:58:37 +0300

    Add files via upload

---
+++ srub_card/js/script.js
@@ -0,0 +1,75 @@
+$(document).ready(function() {
+	$('.burger').click(function(event){
+		$('.burger, .nav').toggleClass('active');
+		$('body').toggleClass('lock');
+	});
+	$('.nav').click(function(event){
+		$('.burger, .nav').removeClass('active');
+		$('body').removeClass('lock');
+	});
+
+// Animation
+const animItems = document.querySelectorAll('._anim-items');
+if (animItems.length > 0) {
+	window.addEventListener('scroll', animOnScroll);
+	function animOnScroll() {
+		for (let index = 0; index < animItems.length; index++) {
+			const animItem = animItems[index];
+			const animItemHeight = animItem.offsetHeight;
+			const animItemOffset = offset(animItem).top;
+			const animStart = 4;
+
+			let animItemPoint = window.innerHeight - animItemHeight / animStart;
+			if (animItemHeight > window.innerHeight) {
+				animItemPoint = window.innerHeight - window.innerHeight / animStart;
+			}
+
+			if ((pageYOffset > animItemOffset - animItemPoint) && pageYOffset < (animItemOffset + animItemHeight)) {
+				animItem.classList.add('_active');
+			} else {
+				animItem.classList.remove('_active')
+			}
+		}
+	}
+	function offset(el) {
+		const rect = el.getBoundingClientRect(),
+			scrollleft = window.pageXOffset || document.documentElement.scrollLeft,
+			scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+		return { top: rect.top + scrollTop, left: rect.left + scrollleft }
+	}
+	setTimeout(function () {
+	animOnScroll();
+	}, 300);
+}
+
+//Объявляем переменные
+const parent_original = document.querySelector('.topmenu');
+const parent = document.querySelector('.nav_mob');
+const item = document.querySelector('.topmenu_contacts');
+//Слушаем изменение размера экрана
+window.addEventListener('resize', move);
+//Функция
+function move(){
+	const viewport_width = Math.max(document.documentElement.clientWidth, window.innerWidth || 0);
+	if (viewport_width <= 575) {
+		if (!item.classList.contains('done')) {
+			parent.insertBefore(item, parent.children[2]);
+			item.classList.add('done');
+		}
+	} else {
+		if (item.classList.contains('done')) {
+			parent_original.insertBefore(item, parent_original.children[2]);
+			item.classList.remove('done');
+		}
+	}
+}
+//Вызываем функцию
+move();
+
+//Accordion
+	$(".project_bild_list ul").prev().click(function() {
+		$(this).parents(".project_bild_list").find("ul").not(this).slideUp().prev().removeClass("bild_active");
+		$(this).next().not(":visible").slideDown().prev().addClass("bild_active");
+	});
+
+});
